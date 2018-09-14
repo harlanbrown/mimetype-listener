80662cfb5bd644244603a8d575d8fa916d300896
Harlan Brown <hbrown@nuxeo.com>
2018-09-14 08:56:23 -0500

    testing

--- src/main/java/org/nuxeo/sample/MimetypeListener.java
+++ src/main/java/org/nuxeo/sample/MimetypeListener.java
@@ -3,10 +3,11 @@ package org.nuxeo.sample;
 import java.util.Arrays;
 import java.util.List;
 
-import org.nuxeo.ecm.core.api.Blob;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.nuxeo.ecm.core.api.Blob;
 import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.RecoverableClientException;
 import org.nuxeo.ecm.core.event.Event;
 import org.nuxeo.ecm.core.event.EventContext;
 import org.nuxeo.ecm.core.event.EventListener;
@@ -37,18 +38,31 @@ public class MimetypeListener implements EventListener {
         if (!(ctx instanceof DocumentEventContext)) {
           return;
         }
+        log.error("HELLO HELLO");
 
         DocumentEventContext docCtx = (DocumentEventContext) ctx;
         DocumentModel doc = docCtx.getSourceDocument();
+        String mimeType = new String();
 
         // Add some logic starting from here.
 
         try {
             Blob blob = (Blob) doc.getPropertyValue("file:content");
-            log.debug(blob.toString());
+            mimeType = blob.getMimeType();
         } catch (Exception e) {
         }
 
+        log.error(mimeType);
+
+        handleExcep(new RecoverableClientException("error","error",null), event);
 
     }
+    public void handleExcep(RecoverableClientException e, Event event){
+        String msg = "Current event " + event.getName() + " would break Quota restriction, rolling back";
+        log.error(msg);
+        log.info(msg);
+        e.addInfo(msg);
+        event.markRollBack("Quota Exceeded", e);
+    }
+
 }
--- src/test/java/org/nuxeo/sample/TestMimetypeListener.java
+++ src/test/java/org/nuxeo/sample/TestMimetypeListener.java
@@ -5,12 +5,26 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 import java.util.List;
+import java.io.Serializable;
+
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import org.junit.Test;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.nuxeo.ecm.core.event.EventService;
 import org.nuxeo.ecm.core.event.impl.EventListenerDescriptor;
 import org.nuxeo.ecm.platform.test.PlatformFeature;
+import org.nuxeo.ecm.core.api.Blob;
+import org.nuxeo.ecm.core.api.Blobs;
+
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+
+import org.nuxeo.ecm.core.api.IdRef;
+
 import org.nuxeo.runtime.test.runner.Deploy;
 import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
@@ -22,10 +36,28 @@ import com.google.inject.Inject;
 @Deploy({"org.nuxeo.sample.mimetype-listener-core"})
 public class TestMimetypeListener {
 
+    private static final Log log = LogFactory.getLog(TestMimetypeListener.class);
+
     protected final List<String> events = Arrays.asList("beforeDocumentModification");
 
     @Inject
+    protected CoreSession session;
+
+    @Inject
     protected EventService s;
+
+    String docId;
+
+    @Before 
+    public void setUp() {
+        DocumentModel ws1 = session.createDocumentModel("/default-domain/workspaces", "ws1", "Workspace");
+        ws1 = session.createDocument(ws1);
+
+        DocumentModel doc = session.createDocumentModel("/default-domain/workspaces/ws1", "default", "File");
+        doc = session.createDocument(doc);
+        session.save();
+        docId = doc.getId();
+    }
 
     @Test
     public void listenerRegistration() {
@@ -33,4 +65,18 @@ public class TestMimetypeListener {
         assertNotNull(listener);
         assertTrue(events.stream().allMatch(listener::acceptEvent));
     }
+
+	@Test
+    public void listenForBlobAttachment() {
+        EventListenerDescriptor listener = s.getEventListener("mimetypelistener");
+        assertNotNull(listener);
+        Blob blob = Blobs.createBlob("Dummy txt", "text/plain", null, "dummy.txt");
+		DocumentModel doc = session.getDocument(new IdRef(docId));
+        doc.setPropertyValue("file:content", (Serializable) blob);
+        session.save();
+        Blob blob2 = (Blob) doc.getPropertyValue("file:content");
+        assertNotNull(blob2);
+
+    }
+
 }
